5e245fb25f69ce1cc8efa5d83a51f3d5633886c2
Sneh Viren Shah <sneh.shah@procdna.com>
2026-05-14 12:59:24 +0530

    Version 14 May- UI 1.0

--- TurboBI Version 0513/tableau_to_pbi_agent/learn/__main__.py
+++
@@ -1,4 +0,0 @@
-"""Allow `python -m tableau_to_pbi_agent.learn ...`."""
-from .cli import main
-import sys
-sys.exit(main())
--- TurboBI Version 0513/tableau_to_pbi_agent/learn/observers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Concrete observers. Each one targets a single pattern."""
-
-from .card_detector          import CardObserver
-from .kpi_dim_label_detector  import KpiDimLabelObserver
-
-__all__ = ["CardObserver", "KpiDimLabelObserver"]
